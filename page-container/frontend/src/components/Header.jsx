8f3db9d486c9d41606ea8062129efda995a28fa2
Shane Lemmer <34974305+S1hane@users.noreply.github.com>
2021-01-19 16:21:52 -0700

    beginning travel notice banner

--- page-container/frontend/src/components/Header.jsx
+++ page-container/frontend/src/components/Header.jsx
@@ -9,15 +9,49 @@ class Header extends React.Component {
     console.log('rendering Header');
     return (
       <div key="Header">
-        Header
-        Navigation
+        <div className="ppr_rup ppr_priv_health_notice_banner_new">
+          <div className="react-container">
+            <div className="">
+              <div className="_298K8dGN">
+                <div className="_1anKb-nl">
+                  <div className="O82K35T8">
+                    <div className="_2dfJVSbd">
+                      <span className="_19xIjkxN">
+                        <span className="HLvj7Lh5 _1jvubpIi _3aNK9c7h">TRAVEL NOTICE:</span>
+                      </span>
+                      <span className="HLvj7Lh5 ciJH8P2l _2s3pPhGm">
+                        <a target="_blank" href="/COVID-2019" className="_3S6pHEQs gZ95jyA4 _38K76hiv">Learn more about COVID-19</a>
+                      </span>
+                    </div>
+                    <button className="Z5j0Wsic" aria-label="Close">
+                      <svg viewBox="0 0 24 24" width="13px" height="13px" className="_2HBN-k68 _14frGWqD">
+                        <path d="M18.293 19.707L12 13.414l-6.293 6.293-1.414-1.414L10.586 12 4.293 5.707l1.414-1.414L12 10.586l6.293-6.293 1.414 1.414L13.414 12l6.293 6.293z">
+                        </path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
         <div className="navigation-links">
           <div className="navigation-container1">
             <div className="navigation-container2">
               <div className="links-container1">
                 <div className="links-container2">
                   <div className="links-container3">
-                    <ul className="nav-menu-items">
+                    <ul className="nav-menu-items global-nav-links-menu">
                       <li className="nav-sub-item">
                         <a href="#" className="nav-link ui_tabs">Tahiti </a>
                       </li>
@@ -66,38 +100,10 @@ class Header extends React.Component {
                           <span className="ui_icon">
                           </span>Rental Cars </a>
                       </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">
-                          <span className="ui_icon">
-                          </span>Add a Place </a>
-                      </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">
-                          <span className="ui_icon">
-                          </span>Travel Forum </a>
-                      </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">
-                          <span className="ui_icon">
-                          </span>Airlines </a>
-                      </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">
-                          <span className="ui_icon">
-                          </span>Best of 2020 </a>
-                      </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">Road Trips </a>
-                      </li>
-                      <li className="nav-sub-item force-more">
-                        <a href="#" className="nav-link ui_tabs">
-                          <span className="ui_icon">
-                          </span>Help Center </a>
-                      </li>
                     </ul>
-                    <ul className="nav-ellipsis">
+                    <ul className="global-nav-links-menu-ellipsis is-top-only">
                       <li className="global-nav-links-ellipsis">
-                        <span className="nav-link ellipsis">
+                        <span className="global-nav-link global-nav-link-2018 ui_tab ellipsis">
                           <span className="ui_icon more-horizontal">
                           </span>
                         </span>
