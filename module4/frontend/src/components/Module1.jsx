18a8b259bf230342bdaa179fa434e25a0fccef00
Shane Lemmer <34974305+S1hane@users.noreply.github.com>
2021-01-27 11:30:09 -0700

    cleanup

---
+++ module4/frontend/src/components/Module1.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+
+class Module1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    // console.log('Rendering Module1');
+    return (
+      <div className="Module1" key="Module1">
+        Module1 Stub
+      </div>
+    );
+  }
+}
+export default Module1;
